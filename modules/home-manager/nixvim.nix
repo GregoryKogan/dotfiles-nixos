364ca3ec404627c52d59a640e78640528a4e9d6a
GregoryKogan <grisha.koganovskiy@gmail.com>
2024-05-05 00:01:07 +0300

    simple nixconfig done

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -12,8 +12,35 @@
   config = lib.mkIf config.nixvim.enable {
     programs.nixvim = {
       enable = true;
+      
+      defaultEditor = true;
 
-      colorschemes.dracula.enable = true;
+      colorschemes.tokyonight.enable = true;
+
+      enableMan = true;
+      viAlias = true;
+      vimAlias = true;
+
+      clipboard = {
+        register = "unnamedplus";
+	providers.wl-copy.enable = true;
+      };
+
+      opts = {
+        autoindent = true;
+	expandtab = true;
+	breakindent = true;
+	smartindent = true;
+
+	autoread = true;
+	colorcolumn = "120";
+	updatetime = 100;
+
+	number = true;
+	relativenumber = true;
+
+	undofile = true;
+      };
     };
   };
 }
